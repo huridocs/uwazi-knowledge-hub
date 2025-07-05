87607336f53d3f8bd39a85b826999457364c9266
Alexandros Philopoulos <aphilop@gmail.com>
2025-07-05 15:41:24 -0500

    Test pre-commit hooks with badly formatted file

---
+++ test-file.ts
@@ -0,0 +1,7 @@
+// Test file with intentional formatting issues
+const badlyFormatted = 'test';
+function poorFormatting(param: string) {
+  return param.toUpperCase();
+}
+
+console.log(poorFormatting(badlyFormatted));
