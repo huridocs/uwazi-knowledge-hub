5fe1b6ff0d3b8a417d8591703a2c4450d964b294
Alexandros Philopoulos <aphilop@gmail.com>
2026-06-11 10:40:30 -0500

    docs(tutorials): add tutorail about buidling first coleection

--- sidebars.ts
+++ sidebars.ts
@@ -21,13 +21,23 @@ const sidebars: SidebarsConfig = {
       link: {
         type: 'generated-index',
         title: 'Using Uwazi tutorials',
-        description: 'Learn how to start using Uwazi!',
+        description: 'Learn how to start using Uwazi.',
       },
       items: [
         'tutorials/take-a-tour-of-uwazi',
         'tutorials/find-what-you-need',
         'tutorials/add-and-connect-an-entity',
       ],
+    },
+    {
+      type: 'category',
+      label: 'Setting up Uwazi',
+      link: {
+        type: 'generated-index',
+        title: 'Setting up Uwazi tutorials',
+        description: 'Learn how to set up your Uwazi instance.',
+      },
+      items: ['tutorials/build-your-first-collection'],
     },
   ],
   howTo: ['how-to/index'],
